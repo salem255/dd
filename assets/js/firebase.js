edfead41102cdd282b5d1b198cc03419efdae9a8
salem255 <39787995+salem255@users.noreply.github.com>
2026-06-18 05:26:32 +0300

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAnBSOnzoYVcd2ElqiNRuMfjrHCyOfozJA",
   authDomain: "dumbs-a44d4.firebaseapp.com",
   projectId: "dumbs-a44d4",
   storageBucket: "dumbs-a44d4.firebasestorage.app",
   messagingSenderId: "513606341460",
-  appId: "1:513606341460:web:08d7a5da49eecdad6b6ec1",
-  measurementId: "G-8ES63KLZQ5"
+  appId: "1:513606341460:web:08d7a5da49eecdad6b6ec1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
